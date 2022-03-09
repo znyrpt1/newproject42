0b72dd610e0321f6174b47fe9157b7eeed68f213
znyrptt1 <99012780+znyrpt1@users.noreply.github.com>
2022-03-09 04:11:17 +0000

    print1

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World to repl master!");
+  res.send("Hello World to repl master oiu!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  console.log('print first blilne')
 });
